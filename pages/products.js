68f93fd20442198c540f39c95b8fbd60929bb916
smeh09 <shauryameh09@gmail.com>
2022-06-21 15:34:04 +0530

    products page

--- pages/products.js
+++ pages/products.js
@@ -1,25 +1,79 @@
 import styles from "../styles/Products.module.css";
 import { server_link } from "../SERVER_LINK.json";
 import Product from "../Components/Product";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 export default function Products({ products }) {
+  const [displayingProducts, setDisplayingProducts] = useState(products);
+
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [price, setPrice] = useState("");
+
+  const filter = () => {
+    if (!(name || type || price)) {
+      setDisplayingProducts(products);
+      return;
+    }
+    const intPrice = parseInt(price);
+    let updatedProducts = [];
+
+    if (name && name.trim().length > 0) {
+      products.forEach((product) => {
+        product.name.toLowerCase().includes(name.toLowerCase())
+          ? updatedProducts.push(product)
+          : {};
+      });
+    }
+    if (type && type.trim().length > 0) {
+      updatedProducts.forEach((product, index) => {
+        !product.type.toLowerCase().includes(type.toLowerCase())
+          ? updatedProducts.splice(index, 1)
+          : {};
+      });
+    }
+    if (intPrice != null) {
+      updatedProducts.forEach((product, index) => {
+        !parseInt(product.price) <= intPrice
+          ? updatedProducts.splice(index, 1)
+          : {};
+      });
+    }
+    setDisplayingProducts(updatedProducts);
+  };
+
   return (
     <div className={styles.productsContainer}>
       <div id={styles.left}>
         <h2 id={styles.heading}>Filter</h2>
         <div id={styles.filters}>
           <div className={styles.inputControl}>
-            <input className={styles.filter} placeholder="Name" />
+            <input
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              className={styles.filter}
+              placeholder="Name"
+            />
           </div>
           <div className={styles.inputControl}>
-            <input className={styles.filter} placeholder="Category" />
+            <input
+              value={type}
+              onChange={(event) => setType(event.target.value)}
+              className={styles.filter}
+              placeholder="Category"
+            />
           </div>
           <div className={styles.inputControl} id={styles.lastInputControl}>
-            <input className={styles.filter} placeholder="Price" />
+            <input
+              value={price}
+              onChange={(event) => setPrice(event.target.value)}
+              className={styles.filter}
+              placeholder="Price"
+            />
           </div>
-          <button id={styles.search}>Search</button>
+          <button id={styles.search} onClick={filter}>
+            Search
+          </button>
         </div>
       </div>
       <div id={styles.right}>
@@ -27,7 +81,7 @@ export default function Products({ products }) {
           Discover
         </h1>
         <div id={styles.products}>
-          {products.map((productData, index) => {
+          {displayingProducts.map((productData, index) => {
             return (
               <Product
                 key={index}
