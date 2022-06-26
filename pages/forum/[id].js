00ade2ec7d65fd736d542cb98cb5756bf6a5b6a3
Tijil Agrawal <77985046+Tijil2111@users.noreply.github.com>
2022-06-26 11:04:46 +0530

    Fix

--- pages/forum/[id].js
+++ pages/forum/[id].js
@@ -1,59 +1,59 @@
-// import styles from "../../styles/Thread.module.css";
+import styles from "../../styles/Thread.module.css";
 
-// export const getStaticPaths = async () => {
-//   const res = await fetch("https://w3btek-backend.herokuapp.com/api/forum");
-//   const data = await res.json();
+export const getStaticPaths = async () => {
+  const res = await fetch("https://w3btek-backend.herokuapp.com/api/forum");
+  const data = await res.json();
 
-//   const paths = data.map((myData) => {
-//     return {
-//       params: { id: myData._id },
-//     };
-//   });
+  const paths = data.map((myData) => {
+    return {
+      params: { id: myData._id },
+    };
+  });
 
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-// export const getStaticProps = async (context) => {
-//   const id = context.params.id;
-//   const res = await fetch(
-//     "https://w3btek-backend.herokuapp.com/api/forum/" + id
-//   );
-//   const data = await res.json();
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(
+    "https://w3btek-backend.herokuapp.com/api/forum/" + id
+  );
+  const data = await res.json();
 
-//   return {
-//     props: { data },
-//   };
-// };
+  return {
+    props: { data },
+  };
+};
 
-// const Details = ({ data }) => {
-//   const answers = data.thread.messages.filter((message, index) => index !== 0);
-//   console.log(answers);
+const Details = ({ data }) => {
+  const answers = data.thread.messages.filter((message, index) => index !== 0);
+  console.log(answers);
 
-//   return (
-//     <div id={styles.container}>
-//       <h1 id={styles.heading}>{data.thread.messages[0]}</h1>
-//       <div className={styles.box}>
-//         <div className={styles.date}>
-//           {`Question asked at ${data.thread.date}`}
-//         </div>
-//         <div className={styles.name}>
-//           {`Question asked by ${data.thread.name}`}
-//         </div>
-//       </div>
-//       <div id={styles.answers}>
-//         {answers.map((answer, index) => {
-//           return (
-//             <div key={index}>
-//               <div className={styles.answer}>{answer}</div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div id={styles.container}>
+      <h1 id={styles.heading}>{data.thread.messages[0]}</h1>
+      <div className={styles.box}>
+        <div className={styles.date}>
+          {`Question asked at ${data.thread.date}`}
+        </div>
+        <div className={styles.name}>
+          {`Question asked by ${data.thread.name}`}
+        </div>
+      </div>
+      <div id={styles.answers}>
+        {answers.map((answer, index) => {
+          return (
+            <div key={index}>
+              <div className={styles.answer}>{answer}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-// export default Details;
+export default Details;
